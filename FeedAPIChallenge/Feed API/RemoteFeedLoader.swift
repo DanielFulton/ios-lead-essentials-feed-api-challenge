c4382102ef2b57242b2200c387ee53367cae0383
Daniel <snukisoft@gmail.com>
2021-05-31 05:06:51 +0900

    Remote feed loader calls completion with invalid data error when HTTP client returns non-200 status code.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -5,22 +5,30 @@
 import Foundation
 
 public final class RemoteFeedLoader: FeedLoader {
-	private let url: URL
-	private let client: HTTPClient
-	
-	public enum Error: Swift.Error {
-		case connectivity
-		case invalidData
-	}
-		
-	public init(url: URL, client: HTTPClient) {
-		self.url = url
-		self.client = client
+  private let url: URL
+  private let client: HTTPClient
+  
+  public enum Error: Swift.Error {
+    case connectivity
+    case invalidData
+  }
+  
+  public init(url: URL, client: HTTPClient) {
+    self.url = url
+    self.client = client
   }
   
   public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-    client.get(from: self.url) { (_) in
-      
+    client.get(from: self.url) { (result) in
+      switch result {
+      case .failure(_):
+        completion(.failure(Error.connectivity))
+      case .success( _, let response):
+        if response.statusCode != 200 {
+          completion(.failure(Error.invalidData))
+          return
+        }
+      }
     }
   }
 }
