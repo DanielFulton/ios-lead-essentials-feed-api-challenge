feb6fa8036bc3eb02f8f02d59a44eabd8a0a54d9
Daniel <snukisoft@gmail.com>
2021-06-01 09:07:02 +0900

    Weak self to prevent memory leaks and refactoring.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,7 +19,8 @@ public final class RemoteFeedLoader: FeedLoader {
   }
   
   public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-    client.get(from: self.url) { (result) in
+    client.get(from: self.url) { [weak self] (result) in
+      guard let self = self else { return }
       switch result {
       case .failure(_):
         completion(.failure(Error.connectivity))
@@ -29,12 +30,51 @@ public final class RemoteFeedLoader: FeedLoader {
           return
         }
         do {
-          let _  = try JSONSerialization.jsonObject(with: data, options: [])
+          let jsonObj = try JSONSerialization.jsonObject(with: data, options: [])
+          if let rootJSONDict = jsonObj as? [String:Any] {
+            if let dicts = rootJSONDict["items"] as? [[String:Any]] {
+              let images = dicts.compactMap({ self.imageForDict($0) })
+              completion(.success(images))
+              return
+            }
+          }
           completion(.success([]))
         } catch {
           completion(.failure(Error.invalidData))
         }
       }
     }
+  }
+  
+  
+  private func imageForDict(_ dict: [String: Any]) -> FeedImage? {
+    guard let uuid = self.uuidForDict(dict) else {
+        return nil
+    }
+    guard let url = self.imageURLforDict(dict) else {
+        return nil
+    }
+    let description = dict["image_desc"] as? String
+    let location = dict["image_loc"] as? String
+    return FeedImage(id: uuid,
+                     description: description,
+                     location: location,
+                     url: url)
+  }
+  
+  private func uuidForDict(_ dict: [String: Any]) -> UUID? {
+    guard let id = dict["image_id"] as? String,
+      let uuid = UUID(uuidString: id) else {
+        return nil
+    }
+    return uuid
+  }
+  
+  private func imageURLforDict(_ dict: [String: Any]) -> URL? {
+    guard let imageURL = dict["image_url"] as? String,
+      let url = URL(string: imageURL) else {
+        return nil
+    }
+    return url
   }
 }
